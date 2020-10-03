8e049de24530082a1b71dbdd55a1374266a445da
ws134 <ws134@duke.edu>
2020-10-03 06:26:45 -0400

    reformat setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-from version import __version__
+from pyfcl.version import __version__
 
 ext_modules=[
-    Extension("pyfcl", ["pyfcl.pyx"], 
-      #@TODO: better include for include/eigen_wrapper.h
-    	include_dirs = ['/usr/local/include', '/usr/include/eigen3', 'include/'],
-        library_dirs = ['/usr/lib', '/usr/local/lib','/usr/local/lib:/opt/ros/lunar/lib'],
-        libraries=["fcl","ccd", "stdc++", "octomap"],
-        language="c++",
-        extra_compile_args = ["-std=c++11", "-O3"])]
+    Extension("pyfcl", ["pyfcl/pyfcl.pyx"], 
+    #@TODO: better include for include/eigen_wrapper.h
+    include_dirs = ['/usr/local/include', '/usr/include/eigen3', 'include/'],
+    library_dirs = ['/usr/lib', '/usr/local/lib','/usr/local/lib:/opt/ros/lunar/lib'],
+    libraries=["fcl","ccd", "stdc++", "octomap"],
+    language="c++",
+    extra_compile_args = ["-std=c++11", "-O3"])]
 
 setup(
   name = 'PyFCL',
