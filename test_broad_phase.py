542b0c605b18fbdd9bd2c6caf9beb86e264d7a03
ws134 <ws134@duke.edu>
2020-10-06 19:56:41 -0400

    more tests on broadphase

--- test_broad_phase.py
+++ test_broad_phase.py
@@ -25,8 +25,67 @@ class TestBroadPhaseCC(unittest.TestCase):
 
         return manager
 
-    def test_self_collision(self):
+    def test_registerObjects(self):
+        # @TODO: This is not a very good test
+        co1 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,0,0])))
+        co2 = fcl.CollisionObject(fcl.Cylinder(0.5, 1), fcl.Transform(np.array([0,0,0,1]), np.array([0,2,0])))
+        co3 = fcl.CollisionObject(fcl.Capsule(0.5, 1), fcl.Transform(np.array([0,0,0,1]), np.array([0,3,0])))
 
+        manager = fcl.DynamicAABBTreeCollisionManager()
+        manager.registerObjects([co1, co2, co3])
+        manager.setup()
+
+        registered_objects = manager.getObjects()
+        self.assertTrue(registered_objects[0] == co1)
+        self.assertTrue(registered_objects[1] == co2)
+        self.assertTrue(registered_objects[2] == co3)
+
+    def test_registerObject(self):
+        co1 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,0,0])))
+        co2 = fcl.CollisionObject(fcl.Cylinder(0.5, 1), fcl.Transform(np.array([0,0,0,1]), np.array([0,2,0])))
+        co3 = fcl.CollisionObject(fcl.Capsule(0.5, 1), fcl.Transform(np.array([0,0,0,1]), np.array([0,3,0])))
+
+        manager = fcl.DynamicAABBTreeCollisionManager()
+        manager.registerObject(co1)
+        manager.registerObject(co2)
+        manager.registerObject(co3)
+        manager.setup()
+
+        registered_objects = manager.getObjects()
+        self.assertTrue(registered_objects[0] == co1)
+        self.assertTrue(registered_objects[1] == co2)
+        self.assertTrue(registered_objects[2] == co3)
+
+    def test_unregisterObject(self):
+        co1 = fcl.CollisionObject(fcl.Sphere(1), fcl.Transform(np.array([0,0,0,1]), np.array([0,0,0])))
+        co2 = fcl.CollisionObject(fcl.Sphere(1), fcl.Transform(np.array([0,0,0,1]), np.array([0,3,0])))
+        co3 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,4,0])))
+
+        manager = fcl.DynamicAABBTreeCollisionManager()
+        manager.registerObjects([co1, co2, co3])
+        manager.setup()
+
+        sphere_object = fcl.CollisionObject(fcl.Sphere(0.51), fcl.Transform(np.array([0,0,0,1]), np.array([0,3,1.5])))
+        cdata = fcl.CollisionData()
+        manager.collide(sphere_object, cdata, fcl.defaultCollisionCallback)
+        self.assertTrue(cdata.result.is_collision == True)
+
+        # After removing co2 from manager, sphere_object will not collide with manager
+        manager.unregisterObject(co2)
+        # @TODO: It seems that update is not needed after register/unregister object(s).
+        manager.update()
+        cdata = fcl.CollisionData()
+        manager.collide(sphere_object, cdata, fcl.defaultCollisionCallback)
+        self.assertTrue(cdata.result.is_collision == False)
+
+        # Let's add co2 back to manager, so that the sphere collides with it
+        manager.registerObject(co2)
+        manager.update()
+        cdata = fcl.CollisionData()
+        manager.collide(sphere_object, cdata, fcl.defaultCollisionCallback)
+        self.assertTrue(cdata.result.is_collision == True)
+
+    def test_self_collision(self):
         manager = self.construct_manager()
 
         cdata = fcl.CollisionData()
@@ -49,4 +108,7 @@ class TestBroadPhaseCC(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
+    tbp = TestBroadPhaseCC()
+    tbp.test_registerObject()
     
