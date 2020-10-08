d1587bb26057ca2afcf9e7d80b359873c3e2acfd
Weidong Sun <me@weidongsun.com>
2020-10-08 15:10:31 +0800

    remove octomap from libraries

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ ext_modules=[
     #@TODO: better include for include/eigen_wrapper.h
     include_dirs = ['/usr/local/include', '/usr/include/eigen3', 'include/', '/usr/local/include/eigen3'],
     library_dirs = ['/usr/lib', '/usr/local/lib','/usr/local/lib:/opt/ros/lunar/lib'],
-    libraries=["fcl","ccd", "stdc++", "octomap"],
+    libraries=["fcl","ccd", "stdc++"],
     language="c++",
     extra_compile_args = ["-std=c++11"])]
 
