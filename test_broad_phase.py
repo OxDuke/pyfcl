0f71061480bc4abe09a6398c77aa42c724d88878
Weidong Sun <me@weidongsun.com>
2020-10-08 15:20:43 +0800

    more tests on broadphase distance

--- test_broad_phase.py
+++ test_broad_phase.py
@@ -101,8 +101,49 @@ class TestBroadPhaseCC(unittest.TestCase):
 
 
     def test_self_distance(self):
-        pass
-        # manager = self.construct_manager()
+        # @TODO: use a helper function to assemble manager
+        co1 = fcl.CollisionObject(fcl.Sphere(1), fcl.Transform(np.array([0,0,0,1]), np.array([0,0,0])))
+        co2 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,4,0])))
+        
+        manager = fcl.DynamicAABBTreeCollisionManager()
+        manager.registerObjects([co1, co2])
+        manager.setup()
+
+        ddata = fcl.DistanceData()
+        manager.distance(ddata, fcl.defaultDistanceCallback)
+        np.testing.assert_allclose(ddata.result.min_distance, 2.5)
+
+    def test_one2many_distance(self):
+        co1 = fcl.CollisionObject(fcl.Sphere(1), fcl.Transform(np.array([0,0,0,1]), np.array([0,0,0])))
+        co2 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,4,0])))
+        
+        manager1 = fcl.DynamicAABBTreeCollisionManager()
+        manager1.registerObjects([co1, co2])
+        manager1.setup()
+
+        sphere_object = fcl.CollisionObject(fcl.Sphere(0.5), fcl.Transform(np.array([0,0,0,1]), np.array([0,2,0])))
+
+        ddata = fcl.DistanceData()
+        manager1.distance(sphere_object, ddata, fcl.defaultDistanceCallback)
+        np.testing.assert_allclose(ddata.result.min_distance, 0.5)
+
+    def test_many2many_distance(self):
+        co1 = fcl.CollisionObject(fcl.Sphere(1), fcl.Transform(np.array([0,0,0,1]), np.array([0,0,0])))
+        co2 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,4,0])))
+        manager1 = fcl.DynamicAABBTreeCollisionManager()
+        manager1.registerObjects([co1, co2])
+        manager1.setup()
+
+        co3 = fcl.CollisionObject(fcl.Sphere(0.5), fcl.Transform(np.array([0,0,0,1]), np.array([0,2,0])))
+        co4 = fcl.CollisionObject(fcl.Box(1,1,1), fcl.Transform(np.array([0,0,0,1]), np.array([0,4,10])))
+        manager2 = fcl.DynamicAABBTreeCollisionManager()
+        manager2.registerObjects([co3, co4])
+        manager2.setup()
+
+        ddata = fcl.DistanceData()
+        manager1.distance(manager2, ddata, fcl.defaultDistanceCallback)
+        np.testing.assert_allclose(ddata.result.min_distance, 0.5)
+
 
 if __name__ == '__main__':
     unittest.main()
