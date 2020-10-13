a47576f8dbe127ef7ea5e3558dcbd556ae79b565
Weidong Sun <me@weidongsun.com>
2020-10-13 21:50:10 +0800

    back to original

--- requirements/build.bash
+++ requirements/build.bash
@@ -3,17 +3,17 @@
 cd libccd
 cmake .
 make -j4
-make install
+sudo make install
 cd ..
 
 cd octomap 
 cmake .
 make -j4
-make install
+sudo make install
 cd ..
 
 cd fcl
 cmake -DBUILD_TESTING=OFF .
 make -j4
-make install
+sudo make install
 cd ..
