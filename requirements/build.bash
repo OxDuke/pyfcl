c0660f2bd2bbae9a8ab647d0ffa815e6553e2782
oxduke <464604837@qq.com>
2020-10-13 17:14:10 +0800

    try remove sudo

--- requirements/build.bash
+++ requirements/build.bash
@@ -1,17 +1,17 @@
 cd libccd
 cmake .
 make -j4
-sudo make install
+make install
 cd ..
 
 cd octomap 
 cmake .
 make -j4
-sudo make install
+make install
 cd ..
 
 cd fcl
 cmake -DBUILD_TESTING=OFF .
 make -j4
-sudo make install
+make install
 cd ..
