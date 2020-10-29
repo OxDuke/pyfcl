765344bd835c22387de2619546ebb69b7f06d52d
oxduke <464604837@qq.com>
2020-10-29 18:31:28 +0800

    comment un-finished unit test

--- test/test_data_types.py
+++ test/test_data_types.py
@@ -75,9 +75,9 @@ class TestTransform(unittest.TestCase):
         np.testing.assert_allclose(tf1.toarray(), tf2.toarray(), rtol=5*1e-6, atol=0)
 
     def test_linear_and_translation(self):
-        rmat, rq, rv = self.generate_random_transform()          
-        tf = fcl.Transform(rq, rv)
-        np.testing.assert_allclose(tf1.toarray(), tf2.toarray(), rtol=5*1e-6, atol=0)
+        # rmat, rq, rv = self.generate_random_transform()          
+        # tf = fcl.Transform(rq, rv)
+        # np.testing.assert_allclose(tf1.toarray(), tf2.toarray(), rtol=5*1e-6, atol=0)
         pass
         #raise NotImplementedError
 
