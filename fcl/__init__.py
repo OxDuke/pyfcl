b546dc5251906c62414584bb62529a75d0c4209f
oxduke <464604837@qq.com>
2020-10-29 16:33:09 +0800

    fix imports in init.py

--- fcl/__init__.py
+++ fcl/__init__.py
@@ -1,10 +1,10 @@
 
-from .pyfcl import Transform, CollisionGeometry, CollisionObject
-from .pyfcl import TriangleP, Box, Sphere, Ellipsoid, Capsule, Cone, Cylinder, Convex, Halfspace, Plane, BVHModel
-# from .pyfcl import OcTree, 
-from .pyfcl import collide, distance
-#from .pyfcl import continuousCollide
-from .pyfcl import DynamicAABBTreeCollisionManager, defaultCollisionCallback, defaultDistanceCallback
+from .fcl import Transform, CollisionGeometry, CollisionObject
+from .fcl import TriangleP, Box, Sphere, Ellipsoid, Capsule, Cone, Cylinder, Convex, Halfspace, Plane, BVHModel
+# from .fcl import OcTree, 
+from .fcl import collide, distance
+#from .fcl import continuousCollide
+from .fcl import DynamicAABBTreeCollisionManager, defaultCollisionCallback, defaultDistanceCallback
 
 from .collision_data import OBJECT_TYPE, NODE_TYPE, GJKSolverType, Contact, CostSource, CollisionRequest, CollisionResult, DistanceRequest, DistanceResult, CollisionData, DistanceData
 #from .collision_data import CCDMotionType, CCDSolverType, ContinuousCollisionRequest, ContinuousCollisionResult, 
