85928f4ac081e87fde29fdcd44aec12430b3e593
bill <weidong.sun@xyzrobotics.ai>
2020-10-29 18:38:03 +0800

    add test_transform.py

---
+++ test/test_transform.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+
+import numpy as np
+import unittest
+
+import tf.transformations as tfm
+
+import fcl
+
+class TestTransform(unittest.TestCase):
+
+    def check_tf(self, ):
+        pass
+
+    def test_constructor(self):
+        pass
