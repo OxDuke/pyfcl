c5ecb80234178b4758eb60b027c970fe5d9b2f1e
Weidong Sun <me@weidongsun.com>
2020-10-31 17:27:59 +0800

    convex vs mesh benchmark

---
+++ benchmark/convex_vs_mesh.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import timeit
+import numpy as np
+
+import trimesh
+
+import fcl
+
+from benchmark import build_bvh_outof_trimesh
+
+def build_convex_outof_trimesh(mesh):
+    pass
