a553985949b00c8ac3e6631981a91863f971f61e
bill <weidong.sun@xyzrobotics.ai>
2020-11-23 16:41:04 +0800

    add test file

---
+++ test/test_octree.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, division
+
+import numpy as np
+import unittest
+
+import fcl
+
+class TestOcTree(unittest.TestCase):
+    """
+    docstring
+    """
+    def test1(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
